72f26af93676217a80051c38f64bee1791a751be
Tak Junseok <tjsuk0220@gmail.com>
2023-09-04 20:48:41 +0900

    Feat: Add maker form view

---
+++ Editor/Editor/MakerFormView.cpp
@@ -0,0 +1,90 @@
+﻿// MakerFormView.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Editor.h"
+#include "MakerFormView.h"
+
+#include "TileMaker.h"
+#include "ObjectMaker.h"
+
+// MakerFormView
+
+IMPLEMENT_DYNCREATE(CMakerFormView, CFormView)
+
+CMakerFormView::CMakerFormView()
+	: CFormView(IDD_MakerFormView)
+{
+
+}
+
+CMakerFormView::~CMakerFormView()
+{
+}
+
+void CMakerFormView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMakerFormView, CFormView)
+	ON_BN_CLICKED(IDC_BUTTON1, &CMakerFormView::OnObjectMakerBnClicked)
+	ON_BN_CLICKED(IDC_BUTTON2, &CMakerFormView::OnTileMakerBnClicked)
+	ON_WM_DESTROY()
+END_MESSAGE_MAP()
+
+
+// MakerFormView 진단
+
+#ifdef _DEBUG
+void CMakerFormView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void CMakerFormView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// MakerFormView 메시지 처리기
+
+
+void CMakerFormView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+
+	// 폼 뷰에 배치된 버튼의 폰트를 변경합니다.
+	m_font.CreatePointFont(128, L"굴림");
+	GetDlgItem(IDC_BUTTON1)->SetFont(&m_font);
+	GetDlgItem(IDC_BUTTON2)->SetFont(&m_font);
+
+	m_tileMaker = new CTileMaker();
+	m_objectMaker = new CObjectMaker();
+}
+
+
+void CMakerFormView::OnObjectMakerBnClicked()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+
+void CMakerFormView::OnTileMakerBnClicked()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+
+void CMakerFormView::OnDestroy()
+{
+	CFormView::OnDestroy();
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	SAFE_DELETE(m_objectMaker);
+	SAFE_DELETE(m_tileMaker);
+}
---
+++ Editor/Editor/MakerFormView.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+
+// MakerFormView 폼 보기
+
+class CTileMaker;
+class CObjectMaker;
+
+class CMakerFormView : public CFormView
+{
+	DECLARE_DYNCREATE(CMakerFormView)
+
+protected:
+	CMakerFormView();           // 동적 만들기에 사용되는 protected 생성자입니다.
+	virtual ~CMakerFormView();
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MakerFormView };
+#endif
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+#ifndef _WIN32_WCE
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+
+private:
+	CFont m_font;
+	CObjectMaker* m_objectMaker = nullptr;
+	CTileMaker* m_tileMaker = nullptr;
+
+public:
+	virtual void OnInitialUpdate();
+	afx_msg void OnObjectMakerBnClicked();
+	afx_msg void OnTileMakerBnClicked();
+	afx_msg void OnDestroy();
+};
+
+

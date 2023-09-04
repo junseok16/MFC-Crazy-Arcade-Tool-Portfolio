96671ce3801db7287f98eeece116fb4a6c367365
Junseok Tak <tjsuk0220@gmail.com>
2023-09-04 09:39:41 +0900

    Feat: Add dialogs

---
+++ Editor/Editor/CObjectMaker.cpp
@@ -0,0 +1,43 @@
+﻿// CObjectMaker.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Editor.h"
+#include "afxdialogex.h"
+#include "CObjectMaker.h"
+
+
+// CObjectMaker 대화 상자
+
+IMPLEMENT_DYNAMIC(CObjectMaker, CDialog)
+
+CObjectMaker::CObjectMaker(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_CObjectMaker, pParent)
+{
+
+}
+
+CObjectMaker::~CObjectMaker()
+{
+}
+
+void CObjectMaker::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CObjectMaker, CDialog)
+	ON_WM_DESTROY()
+END_MESSAGE_MAP()
+
+
+// CObjectMaker 메시지 처리기
+
+
+void CObjectMaker::OnDestroy()
+{
+	CDialog::OnDestroy();
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+}
---
+++ Editor/Editor/CObjectMaker.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CObjectMaker 대화 상자
+
+class CObjectMaker : public CDialog
+{
+	DECLARE_DYNAMIC(CObjectMaker)
+
+public:
+	CObjectMaker(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CObjectMaker();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CObjectMaker };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+
+private:
+	
+
+public:
+	afx_msg void OnDestroy();
+};
